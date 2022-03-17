b2d7543ab9055f19806da3ff838a20d63eec0c67
mattdmv <matija.domovic6@gmail.com>
2022-03-17 23:01:44 +0100

    Connecting frontend and backend server

--- client/main.js
+++ client/main.js
@@ -1,3 +1,50 @@
+Dropzone.autoDiscover = false;
+
+function init() {
+    let dz = new Dropzone("#my-awesome-dropzone", {
+        url: "/",
+        maxFiles: 1,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Some Message",
+        autoProcessQueue: false
+    });
+    
+    dz.on("addedfile", function() {
+        if (dz.files[1]!=null) {
+            dz.removeFile(dz.files[0]);        
+        }
+    });
+
+	dz.on("complete", function (file) {
+        let imageData = file.dataURL;
+
+		var url = "http://127.0.0.1:8000/solve"
+
+		$.post(url, {
+			image_data: imageData
+		}, function(data, status) {
+			console.log(data.Solution);
+
+			if(!data || data.length==0) {
+				$('#error').show();
+			} else {
+				var solution = document.getElementById("solutionHolder");
+
+				solution.innerHTML = "<p>" + data.Solution.toString() + "</p>";
+				$('#solution').show();
+				$('#solutionHolder').show();
+			}
+
+		})
+	});
+
+	$("#solveBtn").on('click', function (e) {
+        dz.processQueue();		
+    });
+
+}
+
+
 function updateList() {
 	const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
 		return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
@@ -8,7 +55,18 @@ function updateList() {
 	document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
 }
 
-updateList();
-window.addEventListener('scroll', () => {
-    updateList();
+
+
+$(document).ready(function() {
+	console.log("Page ready!");
+	$('#solution').hide();
+	$('#solutionHolder').hide();
+	$('#error').hide();
+
+	updateList();
+
+	window.addEventListener('scroll', () => {
+    updateList();});
+	
+	init();
 })
