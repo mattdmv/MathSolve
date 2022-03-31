61d57f0b59b3064e73efb6a671531f499e1dc90c
mattdmv <matija.domovic6@gmail.com>
2022-04-01 01:28:04 +0200

    Added sliding navigation menu for mobile screens

--- client/main.js
+++ client/main.js
@@ -78,4 +78,19 @@ $(document).ready(function() {
     updateList();});
 	
 	init();
+
+	const menu_btn = document.querySelector('.hamburger');
+	const mobile_menu = document.querySelector('.mobile-nav');
+	const cancel_btn = document.querySelector('.cancel')
+
+	menu_btn.addEventListener('click', function () {
+		mobile_menu.classList.toggle('is-active');
+	});
+	cancel_btn.addEventListener('click', function () {
+		mobile_menu.classList.toggle('is-active');
+	});
+	mobile_nav_links.addEventListener('click', function () {
+		mobile_menu.classList.toggle('is-active');
+	});
+
 })
